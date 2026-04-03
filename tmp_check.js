599e17525f50c74ec59344a6acb557181ec6c226
ryanz2k <johnryangomez812@gmail.com>
2026-04-03 16:44:45 +0800

    cleaned extra files

--- tmp_check.js
+++
@@ -1,8 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-
-async function check() {
-  const p = await prisma.product.findFirst();
-  console.log(p);
-}
-check();
